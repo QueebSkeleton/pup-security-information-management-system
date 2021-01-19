c0aaa2e14b275595512e9128b57c8c54d403e05a
QueebSkeleton <queebskeleton@gmail.com>
2021-01-19 16:46:42 +0800

    Initial commit

---
+++ src/main/java/oop/elbisri/pupsims/PupSimsApplication.java
@@ -0,0 +1,15 @@
+package oop.elbisri.pupsims;
+
+/**
+ * PUP-SIMS Application Entry Point.
+ * 
+ * @author Rian Reyes
+ *
+ */
+public class PupSimsApplication {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
