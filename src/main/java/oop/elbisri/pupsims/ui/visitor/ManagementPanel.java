eee3274ba34676b4255e7767e85efda9a9a68a1f
BISMILLA <BISMILLA@192.168.1.7>
2021-02-06 15:09:47 +0800

    UPDATE: Added ManagementPanel for the UI of visitor log

---
+++ src/main/java/oop/elbisri/pupsims/ui/visitor/ManagementPanel.java
@@ -0,0 +1,133 @@
+package oop.elbisri.pupsims.ui.visitor;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class ManagementPanel extends JPanel {
+
+	/**
+	 * Default Serial Version UID (for serializability, not important, placed to remove warnings)
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * The main table of this panel.
+	 */
+	private JTable jtblVisitorLog;
+	
+	/**
+	 * TableModel of the main table.
+	 */
+	//protected PaginatingTableModel visitorTableModel;
+	
+	/**
+	 * Add Form Dialog of this panel.
+	 */
+	protected AddDialog visitorAddDialog;
+	
+	/**
+	 * Construct the panel.
+	 */
+	public ManagementPanel() {
+		// Set background to white
+		setBackground(Color.WHITE);
+		// Set border to EmptyBorder for spacing
+		setBorder(new EmptyBorder(10, 10, 10, 10));
+		// Use BoxLayout to lay the internal 3 panels: Header, Table, Pagination Actions
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		/* jpnlHeader - Header Panel */
+		JPanel jpnlHeader = new JPanel();
+		jpnlHeader.setBorder(new EmptyBorder(0, 0, 10, 0));
+		jpnlHeader.setMinimumSize(new Dimension(10, 45));
+		jpnlHeader.setMaximumSize(new Dimension(32767, 55));
+		jpnlHeader.setBackground(Color.WHITE);
+		add(jpnlHeader);
+		/* END OF jpnlHeader */
+		
+		/* jlblHeader - Main Header Label */
+		jpnlHeader.setLayout(new BoxLayout(jpnlHeader, BoxLayout.X_AXIS));
+		JLabel jlblHeader = new JLabel("Manage Visitor");
+		jlblHeader.setFont(new Font("Segoe UI Semibold", Font.BOLD, 24));
+		jpnlHeader.add(jlblHeader);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		panel.setBackground(Color.WHITE);
+		jpnlHeader.add(panel);
+		
+		JButton jbtnShowAddForm = new JButton("Add");
+		jbtnShowAddForm.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jbtnShowAddForm.addActionListener(event -> {
+			visitorAddDialog.setVisible(true);
+		});
+		panel.add(jbtnShowAddForm);
+		/* END OF jlblHeader */
+		
+		/* jscrlpnVisitorTable - Scrollable Table Panel */
+		JScrollPane jscrlpnVisitorTable = new JScrollPane();
+		add(jscrlpnVisitorTable);
+		/* END OF jscrlpnVisitorTable */
+		
+		/* jtblVisitorLog - Main Panel Table */
+		jtblVisitorLog = new JTable();
+		jtblVisitorLog.setRowHeight(25);
+		jtblVisitorLog.setIntercellSpacing(new Dimension(10, 10));
+		jtblVisitorLog.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+	
+		
+		
+		/* jpnlTablePagination - Panel for Pagination Components */
+		JPanel jpnlTablePagination = new JPanel();
+		jpnlTablePagination.setBorder(new EmptyBorder(10, 0, 0, 0));
+		jpnlTablePagination.setBackground(Color.WHITE);
+		jpnlTablePagination.setMaximumSize(new Dimension(32767, 60));
+		jpnlTablePagination.setMinimumSize(new Dimension(10, 50));
+		add(jpnlTablePagination);
+		jpnlTablePagination.setLayout(new BoxLayout(jpnlTablePagination, BoxLayout.X_AXIS));
+		/* END OF jpnlTablePagination */
+		
+		/* jlblPageSize - Pagination Size Label */
+		JLabel jlblPageSize = new JLabel("Limit:");
+		jlblPageSize.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jpnlTablePagination.add(jlblPageSize);
+		/* END OF jlblPageSize */
+		
+		// Spacing
+		jpnlTablePagination.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		/* jcmbPageSize - Pagination Size Choice Box */
+		JComboBox<String> jcmbPageSize = new JComboBox<>();
+		jcmbPageSize.setMaximumSize(new Dimension(100, 25));
+		jcmbPageSize.setModel(new DefaultComboBoxModel<>(new String[] {"10", "20", "50", "100", "200"}));
+		jcmbPageSize.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		jpnlTablePagination.add(jcmbPageSize);
+		/* END OF jcmbPageSize */
+		
+		// Create the add form dialog
+		visitorAddDialog = new AddDialog();
+		visitorAddDialog.visitorManagementPanel = this;
+	}
+	
+	
+	/**
+	 * Updates the table model and prompts for a JTable redraw.
+	 */
+	
+
+
+}
