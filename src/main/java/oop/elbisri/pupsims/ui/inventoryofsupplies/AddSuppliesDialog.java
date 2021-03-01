a1052229f4d6381386f7412bd1a2e5965255da31
ProfessorEL <60186871+ProfessorEL-psd@users.noreply.github.com>
2021-03-01 13:32:16 +0800

    Did some changes in dialog GUI (changed the jlists into radio buttons) Also added functionality for SAVE and CANCEL Button.

--- src/main/java/oop/elbisri/pupsims/ui/inventoryofsupplies/AddSuppliesDialog.java
+++ src/main/java/oop/elbisri/pupsims/ui/inventoryofsupplies/AddSuppliesDialog.java
@@ -1,17 +1,24 @@
 package oop.elbisri.pupsims.ui.inventoryofsupplies;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.swing.AbstractListModel;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
@@ -34,19 +41,33 @@ public class AddSuppliesDialog extends JDialog {
 	private JTextField jtxtfldItemName;
 	private JTextField jtxtfldQuantity;
 	private JTextField jtxtfldPrice;
+	private JTextField jtxtfldOthers;
+	private JRadioButton jrdbtnStationery; 
+	private JRadioButton jrdbtnConsumables;
+	private JRadioButton jrdbtnOthers;
+	private JRadioButton jrdbtnGoodCondition;
+	private JRadioButton jrdbtnBadCondition;
+	private JRadioButton jrdbtnNeedsAttention;
+	private JRadioButton jrdbtnOutOfStock;
+	
+	
 
 	/**
 	 * Create the dialog.
 	 */
 	public AddSuppliesDialog() {
+		
+		// For referencing later 
+		AddSuppliesDialog thisDialog = this;
+		
 		setTitle("New Supplies");
-		setBounds(100, 100, 429, 321);
+		setBounds(100, 100, 492, 321);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
 		gbl_contentPanel.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 67, 0, 0};
 		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
@@ -98,26 +119,41 @@ public class AddSuppliesDialog extends JDialog {
 			contentPanel.add(jlblItemType, gbc_jlblItemType);
 		}
 		{
-			JList<String> jlstItemType = new JList<>();
-			jlstItemType.setModel(new AbstractListModel<>() {
-				/**
-				 * 
-				 */
-				private static final long serialVersionUID = 1L;
-				String[] values = new String[] {"Stationery", "Consumables ", "Others"};
-				public int getSize() {
-					return values.length;
-				}
-				public String getElementAt(int index) {
-					return values[index];
-				}
-			});
-			GridBagConstraints gbc_jlstItemType = new GridBagConstraints();
-			gbc_jlstItemType.insets = new Insets(0, 0, 5, 0);
-			gbc_jlstItemType.fill = GridBagConstraints.HORIZONTAL;
-			gbc_jlstItemType.gridx = 1;
-			gbc_jlstItemType.gridy = 2;
-			contentPanel.add(jlstItemType, gbc_jlstItemType);
+			JPanel jpnlItemType = new JPanel();
+			GridBagConstraints gbc_jpnlItemType = new GridBagConstraints();
+			gbc_jpnlItemType.insets = new Insets(0, 0, 5, 0);
+			gbc_jpnlItemType.fill = GridBagConstraints.BOTH;
+			gbc_jpnlItemType.gridx = 1;
+			gbc_jpnlItemType.gridy = 2;
+			contentPanel.add(jpnlItemType, gbc_jpnlItemType);
+			jpnlItemType.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			
+			ButtonGroup btngrpItemType = new ButtonGroup();
+			{
+				jrdbtnStationery = new JRadioButton("Stationery");
+				jpnlItemType.add(jrdbtnStationery);
+				btngrpItemType.add(jrdbtnStationery);
+			}
+			{
+				jrdbtnConsumables = new JRadioButton("Consumables");
+				jpnlItemType.add(jrdbtnConsumables);
+				btngrpItemType.add(jrdbtnConsumables);
+			}
+			{
+				Component horizontalStrut = Box.createHorizontalStrut(20);
+				jpnlItemType.add(horizontalStrut);
+			}
+			{
+				jrdbtnOthers = new JRadioButton("Others");
+				jpnlItemType.add(jrdbtnOthers);
+				btngrpItemType.add(jrdbtnOthers);
+			}
+			{
+				jtxtfldOthers = new JTextField();
+				jtxtfldOthers.setEnabled(true);
+				jpnlItemType.add(jtxtfldOthers);
+				jtxtfldOthers.setColumns(10);
+			}
 		}
 		{
 			JLabel jlblQuantity = new JLabel("Quantity:");
@@ -147,26 +183,57 @@ public class AddSuppliesDialog extends JDialog {
 			contentPanel.add(jlblCondition, gbc_jlblCondition);
 		}
 		{
-			JList<String> jlstCondition = new JList<>();
-			jlstCondition.setModel(new AbstractListModel<>() {
-				/**
-				 * 
-				 */
-				private static final long serialVersionUID = 1L;
-				String[] values = new String[] {"Good Condition", "Bad Condition", "Needs Attention", "OUT OF STOCK"};
-				public int getSize() {
-					return values.length;
-				}
-				public String getElementAt(int index) {
-					return values[index];
-				}
-			});
-			GridBagConstraints gbc_jlstCondition = new GridBagConstraints();
-			gbc_jlstCondition.insets = new Insets(0, 0, 5, 0);
-			gbc_jlstCondition.fill = GridBagConstraints.HORIZONTAL;
-			gbc_jlstCondition.gridx = 1;
-			gbc_jlstCondition.gridy = 4;
-			contentPanel.add(jlstCondition, gbc_jlstCondition);
+			JPanel jpnlItemCondition = new JPanel();
+			GridBagConstraints gbc_jpnlItemCondition = new GridBagConstraints();
+			gbc_jpnlItemCondition.insets = new Insets(0, 0, 5, 0);
+			gbc_jpnlItemCondition.fill = GridBagConstraints.VERTICAL;
+			gbc_jpnlItemCondition.gridx = 1;
+			gbc_jpnlItemCondition.gridy = 4;
+			contentPanel.add(jpnlItemCondition, gbc_jpnlItemCondition);
+			GridBagLayout gbl_jpnlItemCondition = new GridBagLayout();
+			gbl_jpnlItemCondition.columnWidths = new int[]{0, 0, 0, 0, 0};
+			gbl_jpnlItemCondition.rowHeights = new int[]{0, 0, 0};
+			gbl_jpnlItemCondition.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_jpnlItemCondition.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+			
+			ButtonGroup btngrpItemCondition = new ButtonGroup();
+			jpnlItemCondition.setLayout(gbl_jpnlItemCondition);
+			{
+				jrdbtnGoodCondition = new JRadioButton("Good Condition");
+				GridBagConstraints gbc_jrdbtnGoodCondition = new GridBagConstraints();
+				gbc_jrdbtnGoodCondition.insets = new Insets(0, 0, 5, 5);
+				gbc_jrdbtnGoodCondition.gridx = 0;
+				gbc_jrdbtnGoodCondition.gridy = 0;
+				jpnlItemCondition.add(jrdbtnGoodCondition, gbc_jrdbtnGoodCondition);
+				btngrpItemCondition.add(jrdbtnGoodCondition);
+			}
+			{
+				jrdbtnBadCondition = new JRadioButton("Bad Condition");
+				GridBagConstraints gbc_jrdbtnBadCondition = new GridBagConstraints();
+				gbc_jrdbtnBadCondition.insets = new Insets(0, 0, 5, 5);
+				gbc_jrdbtnBadCondition.gridx = 1;
+				gbc_jrdbtnBadCondition.gridy = 0;
+				jpnlItemCondition.add(jrdbtnBadCondition, gbc_jrdbtnBadCondition);
+				btngrpItemCondition.add(jrdbtnBadCondition);
+			}
+			{
+				jrdbtnNeedsAttention = new JRadioButton("Needs Attention");
+				GridBagConstraints gbc_jrdbtnNeedsAttention = new GridBagConstraints();
+				gbc_jrdbtnNeedsAttention.insets = new Insets(0, 0, 0, 5);
+				gbc_jrdbtnNeedsAttention.gridx = 0;
+				gbc_jrdbtnNeedsAttention.gridy = 1;
+				jpnlItemCondition.add(jrdbtnNeedsAttention, gbc_jrdbtnNeedsAttention);
+				btngrpItemCondition.add(jrdbtnNeedsAttention);
+			}
+			{
+				jrdbtnOutOfStock = new JRadioButton("OUT OF STOCK");
+				GridBagConstraints gbc_jrdbtnOutOfStock = new GridBagConstraints();
+				gbc_jrdbtnOutOfStock.insets = new Insets(0, 0, 0, 5);
+				gbc_jrdbtnOutOfStock.gridx = 1;
+				gbc_jrdbtnOutOfStock.gridy = 1;
+				jpnlItemCondition.add(jrdbtnOutOfStock, gbc_jrdbtnOutOfStock);
+				btngrpItemCondition.add(jrdbtnOutOfStock);
+			}
 		}
 		{
 			JLabel jlblPrice = new JLabel("Price:");
@@ -191,15 +258,65 @@ public class AddSuppliesDialog extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
+				JButton jbtnSaveButton = new JButton("SAVE");
+				jbtnSaveButton.addActionListener(event -> {
+					try {
+						Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pupsims_db", "pupsims", "pupsimspass_123");
+						Statement statement = connection.createStatement();
+						
+						/*Condition set for item type button group*/
+						
+						//String holder for user input in item type
+						String strItemTypeInput = " "; 
+						
+						if (jrdbtnStationery.isSelected())
+							strItemTypeInput = "Stationery";
+						else if (jrdbtnConsumables.isSelected())
+							strItemTypeInput = "Consumables";
+						else if (jrdbtnOthers.isSelected())
+							strItemTypeInput = jtxtfldOthers.getText();
+						else 
+							strItemTypeInput = "no status selected";
+						
+						/*Condition set for item condition button group*/
+						
+					
+						//String holder for user input in item condition
+						String strItemConditionInput = " ";
+						
+						if (jrdbtnGoodCondition.isSelected())
+							strItemConditionInput = "Good Condition";
+						else if (jrdbtnBadCondition.isSelected())
+							strItemConditionInput = "Bad Condition";
+						else if (jrdbtnNeedsAttention.isSelected())
+							strItemConditionInput = "Needs Attention";
+						else if(jrdbtnOutOfStock.isSelected())
+							strItemConditionInput = "Out Of Stock";
+						else 
+							strItemConditionInput = "no status selected";
+							
+						statement.execute("INSERT INTO inventory_of_supplies VALUES ('" + jtxtfldProductCode.getText() + "','" + jtxtfldItemName.getText() +
+																						"','" + strItemTypeInput + "','" + jtxtfldQuantity.getText() + 
+																						"','" + strItemConditionInput + "','" + jtxtfldPrice.getText() + "')");
+						
+						JOptionPane.showMessageDialog(null, "Supply item successfully saved!");
+					} catch(SQLException e) {
+					  JOptionPane.showMessageDialog(null, "An error occured while saving.\n\n Details:" + e);
+					}
+
+					// Closes this dialog right after creation
+					this.setVisible(false);
+				});
+				buttonPane.add(jbtnSaveButton);
+				}
+				
+				
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton jbtnCancelButton = new JButton("CANCEL");
+				jbtnCancelButton.addActionListener(event -> {
+					thisDialog.setVisible(false);
+				});
+				buttonPane.add(jbtnCancelButton);
 			}
 		}
 	}
